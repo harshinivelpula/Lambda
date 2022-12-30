0cc9e02c4ced7e99427fff7a87bad88c7ebc775c
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-30 16:55:24 +0530

    calling the methods

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LamdaExpressionDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> listPersonInCity = new List<Person>();
+            LambdaDemo.AddRecords(listPersonInCity);
+            LambdaDemo.RetrievingTopTwoRecordForAgelsLessThanSixty(listPersonInCity);
+            LambdaDemo.CheckingForTeenagerPerson(listPersonInCity);
+            LambdaDemo.AllPersonsAverageAge(listPersonInCity);
+            LambdaDemo.CheckNameExistOrNot(listPersonInCity);
+            Console.ReadKey();
+        }
+    }
+}
